7e89b93fa9072139c6d644f75304b2923d1804fa
alekmiltenov <alekmiltenov@gmail.com>
2025-04-17 22:14:39 +0300

    Kamen e travers

--- SparMate_hardware/Servo_code.ino
+++ SparMate_hardware/Servo_code.ino
@@ -9,7 +9,7 @@ Servo rightUppercut;
 
 #include "Combos.h"
 
-#define START_BUTTON_PIN 4
+#define START_BUTTON_PIN     4
 
 #define EASY_BUTTON_PIN     32
 #define NORMAL_BUTTON_PIN   33
@@ -66,10 +66,10 @@ void punch(Servo* punch) {
 }
 
 //======== EXECUTE COMBO ========//
-void fight(int difficulty){
+void fight(int DIFFICULTY){
   int combo = random(0,2);
-  switch(difficulty){
-    case 1: DIFFICULTY_COMBOS = EASY_COMBOS
+  switch(DIFFICULTY){
+    case 1: DIFFICULTY_COMBOS_LIST = EASY_COMBOS
             PUNCH_TIMEOUT = EASY_PUNCH_TIMEOUT
             COMBO_TIMEOUT = EASY_COMBO_TIMEOUT
   }
@@ -82,7 +82,7 @@ void fight(int difficulty){
 }
 
 //======== START/STOP BUTTON ========//
-bool running = false;
+bool running = true; //////////////////////////////////////////////////////////////////CHNAGE THAT YOU STUPID JEW
 void startstop(){
   int STARTSTOP_BUTTON = digitalRead(START_BUTTON_PIN);
   if(STARTSTOP_BUTTON == LOW){
@@ -92,46 +92,38 @@ void startstop(){
 }
 
 //======== DIFFICULTY ========//
-int CHOSEN_COMBOS_DIFF = 0;
+int DIFFICULTY = 0;
 int PUNCH_TIMEOUT;
 int COMBO_TIMEOUT;
 
-int difficulty(){
+int selectDifficulty(){
 
   int EASY_BUTTON = digitalRead(EASY_BUTTON_PIN);
   int NORMAL_BUTTON = digitalRead(NORMAL_BUTTON_PIN);
   int ADVANCED_BUTTON = digitalRead(ADVANCED_BUTTON_PIN);
   int PRO_BUTTON = digitalRead(PRO_BUTTON_PIN);
 
-  while(CHOSEN_COMBOS_DIFF == 0){
+  if(EASY_BUTTON == LOW){
+    DIFFICULTY = 1;
+    PUNCH_TIMEOUT = EASY_PUNCH_TIMEOUT;
+    COMBO_TIMEOUT = EASY_COMBO_TIMEOUT;
+  }
+  if(NORMAL_BUTTON == LOW){
+    DIFFICULTY = 2;
+    PUNCH_TIMEOUT = NORMAL_PUNCH_TIMEOUT;
+    COMBO_TIMEOUT = NORMAL_COMBO_TIMEOUT;
+  }
+  if(ADVANCED_BUTTON == LOW){
+    DIFFICULTY = 3;
+    PUNCH_TIMEOUT = ADVANCED_PUNCH_TIMEOUT;
+    COMBO_TIMEOUT = ADVANCED_COMBO_TIMEOUT;
+  }
+  if(PRO_BUTTON == LOW){
+    DIFFICULTY = 4;
+    PUNCH_TIMEOUT = PRO_PUNCH_TIMEOUT;
+    COMBO_TIMEOUT = PRO_COMBO_TIMEOUT;
+  }
 
-      switch(CHOSEN_COMBOS_DIFF) {
-      case 1:
-        CHOSEN_COMBOS_DIFF = EASY_COMBOS;
-        PUNCH_TIMEOUT = EASY_PUNCH_TIMEOUT;
-        COMBO_TIMEOUT = EASY_COMBO_TIMEOUT;
-        break;
-      case 2:
-        CHOSEN_COMBOS_DIFF = NORMAL_COMBOS;
-        PUNCH_TIMEOUT = NORMAL_PUNCH_TIMEOUT;
-        COMBO_TIMEOUT = NORMAL_COMBO_TIMEOUT;
-        break;
-      case 3:
-        CHOSEN_COMBOS_DIFF = ADVANCED_COMBOS;
-        PUNCH_TIMEOUT = ADVANCED_PUNCH_TIMEOUT;
-        COMBO_TIMEOUT = ADVANCED_COMBO_TIMEOUT;
-        break;
-      case 4:
-        CHOSEN_COMBOS_DIFF = PRO_COMBOS;
-        PUNCH_TIMEOUT = PRO_PUNCH_TIMEOUT;
-        COMBO_TIMEOUT = PRO_COMBO_TIMEOUT;
-        break;
-      default:
-        CHOSEN_COMBOS_DIFF = EASY_COMBOS;
-        PUNCH_TIMEOUT = EASY_PUNCH_TIMEOUT;
-        COMBO_TIMEOUT = EASY_COMBO_TIMEOUT;
-        break;
-    }
   } // while difficulty mode is 0 (not chosen)
 Servo* (*CHOSEN_COMBOS_DIFF)[MAX_MOVES];
 }
@@ -139,9 +131,8 @@ Servo* (*CHOSEN_COMBOS_DIFF)[MAX_MOVES];
 void loop() {
   startstop();
   if(running){
-    difficulty();
+    selectDifficulty();
     fight();
-
   }
   
 }
